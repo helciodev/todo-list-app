a9b6acc4bcf400b7f64f86132836e1b1757f56ce
helciodev <helcioandre@live.com>
2021-05-27 12:36:01 +0100

    Create LOCA_STORAGE_KEY create function saveAndRender

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ ctrlr();
 
 // create elemeents a get elements
 const listContainer = document.getElementById('list-container');
-const lists = [];
+const LOCAL_STORAGE_KEY = 'task.lists';
+const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 const addList = document.getElementById('add-list');
 const listInput = document.getElementById('list-input');
 const formList = document.querySelector('[data-list-new]');
@@ -17,21 +18,31 @@ function clearElement(element) {
     element.removeChild(element.firstChild);
   }
 }
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(lists));
+}
+
 function renderList() {
   clearElement(listContainer);
   lists.forEach((list) => {
     const listElement = document.createElement('li');
     listElement.setAttribute('class', 'text-2xl my-2 text-gray-100 uppercase');
     listElement.dataset.listId = list.id;
-    listElement.textContent = listInput.name;
-    listContainer.appendChild(list);
-    listInput.value = '';
+    listElement.textContent = list.name;
+    listContainer.appendChild(listElement);
   });
+}
+
+function saveAndRender() {
+  save();
+  renderList();
 }
 
 function createList(name) {
   return { id: Date.now().toString(), name, tasks: [] };
 }
+
 formList.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -40,7 +51,8 @@ formList.addEventListener('submit', (event) => {
   if (listName === null || listName === '') return;
 
   const list = createList(listName);
-
+  listInput.value = '';
   lists.push(list);
-  renderList();
+  saveAndRender();
+  console.log(lists);
 });
