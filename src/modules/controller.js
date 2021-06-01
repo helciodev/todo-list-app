f529c1fd18b86fccedaf9c5f4af6b85b7e001d16
helciodev <helcioandre@live.com>
2021-06-01 16:29:23 +0100

    Delete modules folder

--- src/modules/controller.js
+++
@@ -1,5 +0,0 @@
-function ctrlr() {
-  console.log('this the controller module');
-}
-
-export default ctrlr;
--- src/modules/views/todoViews.js
+++
@@ -1,5 +0,0 @@
-function bela() {
-  console.log('this the bela function');
-}
-
-export default bela;
